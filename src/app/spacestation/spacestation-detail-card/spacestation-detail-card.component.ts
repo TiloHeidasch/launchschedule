f7a8f8c44b936a356ceba0e7180bbb87de0f7e19
Tilo <tilo@pop-os.localdomain>
2020-08-05 22:18:07 +0200

    Adds Dockingport interaction

--- src/app/spacestation/spacestation-detail-card/spacestation-detail-card.component.ts
+++ src/app/spacestation/spacestation-detail-card/spacestation-detail-card.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { LaunchLibraryService } from 'src/app/launch-library.service';
+import { ActionSheetController, ToastController } from '@ionic/angular';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'spacestation-detail-card',
@@ -9,10 +11,45 @@ import { LaunchLibraryService } from 'src/app/launch-library.service';
 export class SpacestationDetailCardComponent implements OnInit {
   @Input('spacestation') spacestation;
 
-  constructor(private service: LaunchLibraryService) {
+  constructor(private service: LaunchLibraryService, public actionSheetController: ActionSheetController, public toastController: ToastController, private router: Router) {
   }
 
   async ngOnInit() {
   }
 
+  async presentUnoccupiedToast(dockingLocation) {
+    const toast = await this.toastController.create({
+      message: dockingLocation.name + ' is not occupied.',
+      duration: 2000
+    });
+    toast.present();
+  }
+
+  async presentDockingLocationActionSheet(dockingLocation) {
+    const actionSheet = await this.actionSheetController.create({
+      header: 'Albums',
+      cssClass: 'my-custom-class',
+      buttons: [{
+        text: 'Goto Launch',
+        icon: 'rocket',
+        handler: () => {
+          this.router.navigate(['launch/' + dockingLocation.docked.flight_vehicle.launch.id]);
+        }
+      }, {
+        text: 'Goto Spacecraft',
+        icon: 'planet',
+        handler: () => {
+          this.router.navigate(['spacecraft/' + dockingLocation.docked.flight_vehicle.spacecraft.spacecraft_config.id]);
+        }
+      }, {
+        text: 'Cancel',
+        icon: 'close',
+        role: 'cancel',
+        handler: () => {
+          console.log('Cancel clicked');
+        }
+      }]
+    });
+    await actionSheet.present();
+  }
 }
