079459eb896c3faff30faedcc4954a58ea67fd31
Tilo <tilo@pop-os.localdomain>
2020-08-16 17:31:28 +0200

    Fixes no such file or directory

--- fetchLaunches.ts
+++ fetchLaunches.ts
@@ -3,7 +3,7 @@ var fs = require('fs');
 
 var dataAll = [];
 function requestUrlAndPersistToFile(url, filename) {
-    console.log(filename + ' requesting ' + url);
+    console.log(process.argv[1] + ' requesting ' + url);
 
     https.get(url, (resp) => {
         let data = '';
@@ -17,8 +17,8 @@ function requestUrlAndPersistToFile(url, filename) {
         resp.on('end', () => {
             const recieved = JSON.parse(data);
             dataAll = dataAll.concat(recieved.results);
-            console.log(filename + ' recieved ' + recieved.results.length + ' records');
-            console.log(filename + ' dataAll.length ' + dataAll.length);
+            console.log(process.argv[1] + ' recieved ' + recieved.results.length + ' records');
+            console.log(process.argv[1] + ' dataAll.length ' + dataAll.length);
             if (recieved.next) {
                 requestUrlAndPersistToFile(recieved.next, filename);
             } else {
@@ -32,10 +32,14 @@ function requestUrlAndPersistToFile(url, filename) {
 }
 function persistData(filename) {
     const data = prepareData();
-    console.log(filename + ' writing file');
-    fs.writeFile('src/app/data/' + filename + '.json', JSON.stringify(data), function (err) {
+    if (!fs.existsSync(dir)) {
+        console.log(process.argv[1] + ' creating directory');
+        fs.mkdirSync(dir);
+    }
+    console.log(process.argv[1] + ' writing file');
+    fs.writeFile(dir + filename + '.json', JSON.stringify(data), function (err) {
         if (err) return console.log(err);
-        console.log(filename + ' wrote file');
+        console.log(process.argv[1] + ' wrote file');
         dataAll = [];
     });
 }
@@ -53,6 +57,7 @@ function prepareData() {
     });
 }
 var path = 'launch';
+var dir = 'src/app/data/'
 var fileName = 'launches';
 
 requestUrlAndPersistToFile('https://ll.thespacedevs.com/2.0.0/' + path + '/?format=json&limit=100', fileName);
