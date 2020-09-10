d9adb64aa0da9190057c155c223c673f504b6883
Tilo <tilo@pop-os.localdomain>
2020-09-10 22:34:37 +0200

    Fixes 'ion-icon' is not a known element

--- src/app/notification/notification.module.ts
+++ src/app/notification/notification.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { NotificationIconComponent } from "./notification-icon.component";
+import { IonicModule } from "@ionic/angular";
 
 @NgModule({
   declarations: [NotificationIconComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, IonicModule],
   exports: [NotificationIconComponent],
 })
 export class NotificationModule {}
