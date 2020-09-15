5df4a41e8833a98a2e4e8a5d18cfa7e61cb29720
Tilo Heidasch <tilo.heidasch@capgemini.com>
2020-09-15 18:14:55 +0200

    Adds test data for failing test cases

--- src/app/location/location-detail-card/location-detail-card.component.spec.ts
+++ src/app/location/location-detail-card/location-detail-card.component.spec.ts
@@ -21,6 +21,16 @@ describe("LocationDetailCardComponent", () => {
 
     fixture = TestBed.createComponent(LocationDetailCardComponent);
     component = fixture.componentInstance;
+    component.location = {
+      id: 151,
+      url: "https://ll.thespacedevs.com/2.0.0/location/151/",
+      name: "Broglio Space Center, Kenya",
+      country_code: "ITA",
+      map_image:
+        "https://spacelaunchnow-prod-east.nyc3.digitaloceanspaces.com/media/launch_images/location_151_20200803142409.jpg",
+      total_launch_count: 0,
+      total_landing_count: 0,
+    };
     fixture.detectChanges();
   }));
 
--- src/app/pad/pad-list-entry/pad-list-entry.component.spec.ts
+++ src/app/pad/pad-list-entry/pad-list-entry.component.spec.ts
@@ -21,6 +21,31 @@ describe("PadListEntryComponent", () => {
 
     fixture = TestBed.createComponent(PadListEntryComponent);
     component = fixture.componentInstance;
+    component.pad = {
+      id: 97,
+      url: "https://ll.thespacedevs.com/2.0.0/pad/97/",
+      agency_id: 161,
+      name: "Space Launch Complex 26B",
+      info_url: null,
+      wiki_url:
+        "https://en.wikipedia.org/wiki/Cape_Canaveral_Air_Force_Station_Launch_Complex_26",
+      map_url: "http://www.google.com/maps?q=28.4433,-80.5712",
+      latitude: "28.4433",
+      longitude: "-80.5712",
+      location: {
+        id: 12,
+        url: "https://ll.thespacedevs.com/2.0.0/location/12/",
+        name: "Cape Canaveral, FL, USA",
+        country_code: "USA",
+        map_image:
+          "https://spacelaunchnow-prod-east.nyc3.digitaloceanspaces.com/media/launch_images/location_12_20200803142519.jpg",
+        total_launch_count: 202,
+        total_landing_count: 0,
+      },
+      map_image:
+        "https://spacelaunchnow-prod-east.nyc3.digitaloceanspaces.com/media/launch_images/pad_97_20200803143224.jpg",
+      total_launch_count: 3,
+    };
     fixture.detectChanges();
   }));
 
