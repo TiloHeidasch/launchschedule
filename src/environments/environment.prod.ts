25e4bd201487cc8b8b76243517fc8675a71ea618
Tilo <tilo@pop-os.localdomain>
2020-09-20 14:29:56 +0200

    Fixes failing tests and build

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
   launchLibrary: "https://ll.thespacedevs.com",
+  snapi: "https://spaceflightnewsapi.net/api/v1/",
   notificationUrl: "https://launchschedule-notifications.th105.de/notification",
 };
