44bc5cde0753333e6eadc623e16d1d4edbe521ce
Heidasch <tilo.heidasch@capgemini.com>
2020-09-29 21:21:27 +0200

    Fixes Lint

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -1,8 +1,7 @@
 import { browser, by, element } from "protractor";
 
 describe("new App", () => {
-  it("should have a title", function () {
-    browser.ignoreSynchronization = true;
+  it("should have a title", () => {
     browser.get("/#/launch");
     browser.refresh();
     expect(browser.getTitle()).toEqual("Launchschedule");
