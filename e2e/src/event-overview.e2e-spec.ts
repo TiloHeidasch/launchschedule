9bfff7980244d97af4dcff5b457b00c46607304c
Heidasch <tilo.heidasch@capgemini.com>
2020-10-02 17:29:22 +0200

    Ignore Synchronization

--- e2e/src/event-overview.e2e-spec.ts
+++ e2e/src/event-overview.e2e-spec.ts
@@ -5,6 +5,7 @@ describe("Event", () => {
     browser.get("/#/event");
     browser.refresh();
     browser.sleep(2000);
+    browser.ignoreSynchronization = true;
   });
   it("should have a page title", () => {
     expect(element(by.id("title")).getText()).toBe("Events");
--- e2e/src/launch-overview.e2e-spec.ts
+++ e2e/src/launch-overview.e2e-spec.ts
@@ -5,6 +5,7 @@ describe("Launch", () => {
     browser.get("/#/launch");
     browser.refresh();
     browser.sleep(2000);
+    browser.ignoreSynchronization = true;
   });
   it("should have a page title", () => {
     expect(element(by.id("title")).getText()).toBe("Launches");
