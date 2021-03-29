06ee12c51b1c933be22b44c8840b3ee6a22d135f
Tilo Heidasch <tilo.heidasch@capgemini.com>
2021-03-29 20:24:25 +0200

    Fixes failing test case

--- src/app/nasa-images/rover/mars-photos.service.spec.ts
+++ src/app/nasa-images/rover/mars-photos.service.spec.ts
@@ -5,7 +5,7 @@ import {
 import { TestBed } from "@angular/core/testing";
 import { IonicModule } from "@ionic/angular";
 
-import { MarsPhotosService } from "./mars-photos.service";
+import { MarsPhotosService, Rover } from "./mars-photos.service";
 
 describe("MarsPhotosService", () => {
   let service: MarsPhotosService;
@@ -24,20 +24,19 @@ describe("MarsPhotosService", () => {
     expect(service).toBeTruthy();
   });
   describe("Curiosity Mars Photos", () => {
-    it("should call getLatestCuriosityPhotos", () => {
-      service.getLatestCuriosityPhotos();
-      const today = new Date();
-      const year = today.getFullYear();
-      const month = today.getMonth() + 1;
-      const day = today.getDate();
+    it("should call getPhotosForSol", () => {
+      service.getPhotosForSol(Rover.CURIOSITY, 1);
       const res = httpTestingController.expectOne(
-        "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=" +
-          year +
-          "-" +
-          month +
-          "-" +
-          day +
-          "&api_key=DEMO_KEY"
+        "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1&api_key=DEMO_KEY"
+      );
+      expect(res.request.method).toBe("GET");
+    });
+  });
+  describe("Perseverance Mars Photos", () => {
+    it("should call getPhotosForSol", () => {
+      service.getPhotosForSol(Rover.PERSEVERANCE, 1);
+      const res = httpTestingController.expectOne(
+        "https://api.nasa.gov/mars-photos/api/v1/rovers/perseverance/photos?sol=1&api_key=DEMO_KEY"
       );
       expect(res.request.method).toBe("GET");
     });
