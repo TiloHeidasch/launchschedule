8de85ceb344dc0d6b2c8043c7cdc7a61992b3305
Tilo Heidasch <tilo.heidasch@capgemini.com>
2022-01-26 13:21:11 +0100

    Removes unused imports

--- android/app/src/main/java/de/th105/launchschedule/MainActivity.java
+++ android/app/src/main/java/de/th105/launchschedule/MainActivity.java
@@ -1,10 +1,5 @@
 package de.th105.launchschedule;
 
-import android.os.Bundle;
-
 import com.getcapacitor.BridgeActivity;
-import com.getcapacitor.Plugin;
-
-import java.util.ArrayList;
 
 public class MainActivity extends BridgeActivity {}
