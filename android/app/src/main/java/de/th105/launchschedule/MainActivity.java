7532993d50f662b7ff1bfbdfead6a7f5b5545700
Tilo Heidasch <tilo.heidasch@capgemini.com>
2022-01-26 10:57:06 +0100

    Adds PushNotification Import to android

--- android/app/src/main/java/de/th105/launchschedule/MainActivity.java
+++ android/app/src/main/java/de/th105/launchschedule/MainActivity.java
@@ -7,8 +7,6 @@ import com.getcapacitor.Plugin;
 
 import java.util.ArrayList;
 
-import com.capacitorjs.plugins.storage.StoragePlugin;
-
 public class MainActivity extends BridgeActivity {
   @Override
   public void onCreate(Bundle savedInstanceState) {
@@ -18,7 +16,8 @@ public class MainActivity extends BridgeActivity {
     this.init(savedInstanceState, new ArrayList<Class<? extends Plugin>>() {{
       // Additional plugins you've installed go here
       // Ex: add(TotallyAwesomePlugin.class);
-          registerPlugin(StoragePlugin.class);
+          add(com.capacitorjs.plugins.storage.StoragePlugin.class);
+          add(com.capacitorjs.plugins.pushnotifications.PushNotificationsPlugin.class);
     }});
   }
 }
