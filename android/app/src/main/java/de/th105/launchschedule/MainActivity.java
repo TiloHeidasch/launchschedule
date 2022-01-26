65f09420d0298bb8394567ba37dc0e5f4ff49fde
Tilo Heidasch <tilo.heidasch@capgemini.com>
2022-01-26 13:18:22 +0100

    Removes Implementation

--- android/app/src/main/java/de/th105/launchschedule/MainActivity.java
+++ android/app/src/main/java/de/th105/launchschedule/MainActivity.java
@@ -7,17 +7,4 @@ import com.getcapacitor.Plugin;
 
 import java.util.ArrayList;
 
-public class MainActivity extends BridgeActivity {
-  @Override
-  public void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-
-    // Initializes the Bridge
-    this.init(savedInstanceState, new ArrayList<Class<? extends Plugin>>() {{
-      // Additional plugins you've installed go here
-      // Ex: add(TotallyAwesomePlugin.class);
-          add(com.capacitorjs.plugins.storage.StoragePlugin.class);
-          add(com.capacitorjs.plugins.pushnotifications.PushNotificationsPlugin.class);
-    }});
-  }
-}
+public class MainActivity extends BridgeActivity {}
