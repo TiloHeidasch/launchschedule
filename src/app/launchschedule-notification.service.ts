1581582558e09c1fe72c357b467333728fab4cea
Tilo Heidasch <tilo.heidasch@capgemini.com>
2022-02-01 11:50:14 +0100

    Adds unsubscribe from topic

--- src/app/launchschedule-notification.service.ts
+++ src/app/launchschedule-notification.service.ts
@@ -13,6 +13,7 @@ const notificationDisabled = "disabled";
 export class LaunchscheduleNotificationService {
   private registered = false;
   constructor(private storage: StorageService) {}
+
   async markInterest(type, id): Promise<void> {
     if (this.registered) {
       const key = notificationKey + type + "." + id;
@@ -25,8 +26,14 @@ export class LaunchscheduleNotificationService {
   }
 
   async removeInterest(type, id): Promise<void> {
-    const key = notificationKey + type + "." + id;
-    await this.storage.setItem(key, notificationDisabled);
+    if (this.registered) {
+      const key = notificationKey + type + "." + id;
+      const topic = type + id;
+      await this.storage.setItem(key, notificationDisabled);
+      FCM.unsubscribeFrom({ topic }).catch((err) => {
+        console.log(err);
+      });
+    }
   }
 
   async isInterested(type, id): Promise<boolean> {
