b729f41b213730c0387b9cb629b7ec4371dd15ba
Tilo Heidasch <tilo.heidasch@capgemini.com>
2022-08-04 11:16:16 +0200

    Updates to angular 14.1.1 and capacitor/storage to preferences

--- src/app/storage.service.ts
+++ src/app/storage.service.ts
@@ -1,38 +1,38 @@
 import { Injectable } from "@angular/core";
-import { Storage } from "@capacitor/storage";
+import { Preferences } from "@capacitor/preferences";
 
 @Injectable({
   providedIn: "root",
 })
 export class StorageService {
   async getObject(key: string) {
-    const ret = await Storage.get({ key });
+    const ret = await Preferences.get({ key });
     return JSON.parse(ret.value);
   }
 
   async setObject(key: string, object: any) {
-    await Storage.set({
+    await Preferences.set({
       key,
       value: JSON.stringify(object),
     });
   }
   async setItem(key: string, value: any) {
-    await Storage.set({
+    await Preferences.set({
       key,
       value,
     });
   }
 
   async getItem(key: string) {
-    const { value } = await Storage.get({ key });
+    const { value } = await Preferences.get({ key });
     return value;
   }
 
   async removeItem(key: string) {
-    await Storage.remove({ key });
+    await Preferences.remove({ key });
   }
 
   async clear() {
-    await Storage.clear();
+    await Preferences.clear();
   }
 }
