3ded09ce99bfc23df17c2e42e818024492970cfe
TiloHeidasch <theidasch@gmail.com>
2026-07-17 15:03:51 +0200

    Update NASA API key

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   launchLibrary: "https://ll.thespacedevs.com/2.3.0",
   snapi: "https://api.spaceflightnewsapi.net/v4/",
-  nasaApiKey: "0peEeuH83lQajE5wpCQVFkIhR3MGZKk5ccvXprMY",
+  nasaApiKey: "OKDDjJfgrM8IzBL7Em8F3KAlayHEjPUQ2UEYTjtc",
   twitter: "https://publish.twitter.com/oembed",
   cors: "https://cors.th105.de/",
   versionCode: 1,
