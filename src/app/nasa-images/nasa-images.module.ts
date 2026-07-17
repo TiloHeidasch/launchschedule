a36a2fcaef206027a721f50fa29c19ecfaeaf1f7
TiloHeidasch <theidasch@gmail.com>
2026-07-17 15:19:44 +0200

    fix: resolve NG0201 on /nasa/astro by providing ModalController in NasaImagesModule
    Root cause: @ionic/angular and @ionic/angular/standalone define separate
    ModalController classes. IonicModule.forRoot() provides the former, but
    ngx-ionic-image-viewer injects the latter. Angular DI compares tokens by
    reference, so the injection never matched. Providing ModalController from
    @ionic/angular/standalone in the lazy module resolves the mismatch.
    
    Also removes the irrelevant HttpClientModule added in the previous attempt.

--- src/app/nasa-images/nasa-images.module.ts
+++ src/app/nasa-images/nasa-images.module.ts
@@ -1,16 +1,17 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { FormsModule } from "@angular/forms";
-import { HttpClientModule } from "@angular/common/http";
 
 import { IonicModule } from "@ionic/angular";
+import { ModalController } from "@ionic/angular/standalone";
 
 import { NasaImagesRoutingModule } from "./nasa-images-routing.module";
 
 import { NasaImagesPage } from "./nasa-images.page";
 
 @NgModule({
-  imports: [CommonModule, FormsModule, IonicModule, NasaImagesRoutingModule, HttpClientModule],
+  imports: [CommonModule, FormsModule, IonicModule, NasaImagesRoutingModule],
   declarations: [NasaImagesPage],
+  providers: [ModalController],
 })
 export class NasaImagesModule {}
